dfa630fce65d7b3289cdac81e57d8053c57238a6
Daniel Holström <danielholstrom@gmail.com>
2024-12-20 16:36:29 +0100

    asdf

--- setup_plugins.sh
+++ setup_plugins.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-
+# 1. copy lua file to lua plugins folder
+cp live-share.lua /home/vagrant/.config/nvim/lua/plugins/live-share.lua
+echo "live-share.lua copied to lua folder"
+# 2. run username prompt to set username for live-share nvim.
+echo "running username prompt for nvim live share"
+source ./live_share_set_user.sh
+echo "Success"
